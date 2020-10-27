e6f1dc87369aa8694b49740e0515e8eed2be286d
samupit <samu_pitkanen93@hotmail.com>
2020-10-27 20:12:07 +0200

    created GreetingController.java and GreetingService.java

---
+++ src/main/java/anzsjop_portfolio/portfolio/GreetingService.java
@@ -0,0 +1,10 @@
+package anzsjop_portfolio.portfolio;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+	public String greet() {
+		return "Hei Taavi!";
+	}
+}
