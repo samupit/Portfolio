3f69d27b669ed85bb4a32356e2bca3dd907c4160
AntNar <narvanen.antti@gmail.com>
2020-10-27 19:15:16 +0200

    Added code to SpotifyControllerTest

--- src/test/java/anzsjop_portfolio/portfolio/SpotifyControllerTest.java
+++ src/test/java/anzsjop_portfolio/portfolio/SpotifyControllerTest.java
@@ -1,15 +1,20 @@
 package anzsjop_portfolio.portfolio;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-class SpotifyControllerTest {
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpotifyControllerTest {
 	
 	@Autowired
 	SpotifyController spotifyController;
 
 	@Test
 	void contextLoads() {
-		
+		assertThat(spotifyController).isNotNull();
 	}
 }
