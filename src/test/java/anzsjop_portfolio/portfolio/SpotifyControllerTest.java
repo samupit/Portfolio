3b4d883394347f3c01b0a3c160e811e84066a932
AntNar <narvanen.antti@gmail.com>
2020-10-27 18:53:27 +0200

    Fixed Samus mistakes and added Autowires to SpotifyControllerTest

--- src/test/java/anzsjop_portfolio/portfolio/SpotifyControllerTest.java
+++ src/test/java/anzsjop_portfolio/portfolio/SpotifyControllerTest.java
@@ -1,11 +1,15 @@
 package anzsjop_portfolio.portfolio;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 class SpotifyControllerTest {
 	
+	@Autowired
+	SpotifyController spotifyController;
+
 	@Test
 	void contextLoads() {
-		SpotifyController.greeting();
+		
 	}
 }
