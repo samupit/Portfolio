af5a8bce0abfc0c766377946574b5de9ac781c81
samupit <samu_pitkanen93@hotmail.com>
2020-11-17 20:48:18 +0200

    working on client credentials flow

--- src/main/java/anzsjop_portfolio/portfolio/spotify/authorization_code/SpotifyController.java
+++ src/main/java/anzsjop_portfolio/portfolio/spotify/authorization_code/SpotifyController.java
@@ -1,47 +1,48 @@
 package anzsjop_portfolio.portfolio.spotify.authorization_code;
 
 
+import java.util.Base64;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
 
-
 @Controller
 public class SpotifyController {
 
     private static final String client_id = "c251e8621f594d1b9103deede890cbbc";
-    // private static final String clientSecret = "25d0dd118b3649b68a1b27f3aace65c6";
+    private static final String clientSecret = "25d0dd118b3649b68a1b27f3aace65c6";
     private static final String redirect_uri = "https://example.com/callback";
 
     @Autowired
     WebClient.Builder getWebClientBuilder;
 
-    @RequestMapping(value = "/", method=RequestMethod.GET)
+   /* @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     public String logInButton() {
-        return "<html>\n" + "<header><title>Welcome to the login page </title></header>\n" +
-          "<body>\n" + "Log in\n" + "</body>\n" + "</html>";
-          
-    }
+        return "<html>\n" + "<header><title>Welcome to the login page </title></header>\n" + "<body>\n" + "Log in\n"
+                + "</body>\n" + "</html>";
 
-    @RequestMapping(value = "/authorize", method=RequestMethod.GET)
+    }*/
+
+    @RequestMapping("/api/token")
     public @ResponseBody String spotifyAuthorization() {
-        return getWebClientBuilder.build()
-            .method(HttpMethod.GET)
-            .uri("/authorize?"+
-                "client_id="+ client_id + 
-                "&response_type=code"+
-                "&redirect_uri="+ redirect_uri) 
-            .retrieve()
-            .bodyToMono(String.class)
-            .block();
+        return spotifyCommand;    
     }
 
+        
+    private static final String clientCredentials = "c251e8621f594d1b9103deede890cbbc:25d0dd118b3649b68a1b27f3aace65c6";
+    //private String encodedClient_id = Base64.getEncoder().encodeToString(client_id.getBytes());
+    private String encodedClientCredentials = Base64.getEncoder().encodeToString(clientCredentials.getBytes());
+
+    
+    public String spotifyCommand = "curl -H Content-Type: application/x-www-form-urlencoded" +
+    " -X POST https://accounts.spotify.com/api/token -d grant_type=client_credentials" + 
+    " -H Authorization: Basic " + encodedClientCredentials;
 
     @Bean
     public WebClient.Builder getWebClientBuilder() {
