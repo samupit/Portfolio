e2d278b306d8a42431e2d88d9131257b2bd87712
samupit <samu_pitkanen93@hotmail.com>
2020-11-18 17:25:05 +0200

    cleaned up SpotifyController

--- src/main/java/anzsjop_portfolio/portfolio/spotify/authorization_code/SpotifyController.java
+++ src/main/java/anzsjop_portfolio/portfolio/spotify/authorization_code/SpotifyController.java
@@ -1,6 +1,5 @@
 package anzsjop_portfolio.portfolio.spotify.authorization_code;
 
-
 import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,33 +15,17 @@ public class SpotifyController {
 
     private static final String client_id = "c251e8621f594d1b9103deede890cbbc";
     private static final String clientSecret = "25d0dd118b3649b68a1b27f3aace65c6";
-    private static final String redirect_uri = "https://example.com/callback";
 
     @Autowired
     WebClient.Builder getWebClientBuilder;
 
-   /* @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
-    public String logInButton() {
-        return "<html>\n" + "<header><title>Welcome to the login page </title></header>\n" + "<body>\n" + "Log in\n"
-                + "</body>\n" + "</html>";
-
-    }*/
-
-    @RequestMapping("/api/token")
+    @RequestMapping(value="/spotify/token", method=RequestMethod.GET) // this is our application layer
     public @ResponseBody String spotifyAuthorization() {
-        return spotifyCommand;    
+        String clientCredentials = client_id + ":" + clientSecret;
+        //private String encodedClient_id = Base64.getEncoder().encodeToString(client_id.getBytes());
+        String encodedClientCredentials = Base64.getEncoder().encodeToString(clientCredentials.getBytes());
+        return "Hello World";    
     }
-
-        
-    private static final String clientCredentials = "c251e8621f594d1b9103deede890cbbc:25d0dd118b3649b68a1b27f3aace65c6";
-    //private String encodedClient_id = Base64.getEncoder().encodeToString(client_id.getBytes());
-    private String encodedClientCredentials = Base64.getEncoder().encodeToString(clientCredentials.getBytes());
-
-    
-    public String spotifyCommand = "curl -H Content-Type: application/x-www-form-urlencoded" +
-    " -X POST https://accounts.spotify.com/api/token -d grant_type=client_credentials" + 
-    " -H Authorization: Basic " + encodedClientCredentials;
 
     @Bean
     public WebClient.Builder getWebClientBuilder() {
@@ -52,4 +35,3 @@ public class SpotifyController {
     
 }
     
-
