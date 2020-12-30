2184b6ebc740fd3c1c4d96d93c7e0903ee455ae8
samupit <samu_pitkanen93@hotmail.com>
2020-12-30 09:47:19 +0200

    configured basic settings and dependencies for an in-memory H2 Database for access token storage

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TBL_TOKENS (access_token, token_type, expires_in, scope) VALUES
+    ('BQDlPBW4yeqDKbfLMGb7jrEAIj9gc770', 'Bearer', '3600', ''),
+    ('BQDlPBW4yeqDKbfLMGb7jrEAIj9gc770', 'Bearer', '3600', ''),
+    ('BQDlPBW4yeqDKbfLMGb7jrEAIj9gc770', 'Bearer', '3600', '');
