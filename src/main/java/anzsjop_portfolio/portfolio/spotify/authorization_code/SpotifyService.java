f6f2ea25836b1b170fbd051112a6051fb6062e20
samupit <samu_pitkanen93@hotmail.com>
2021-02-04 19:38:18 +0200

    created a getNewestToken method to SpotifyService

--- src/main/java/anzsjop_portfolio/portfolio/spotify/authorization_code/SpotifyService.java
+++ src/main/java/anzsjop_portfolio/portfolio/spotify/authorization_code/SpotifyService.java
@@ -1,6 +1,9 @@
 package anzsjop_portfolio.portfolio.spotify.authorization_code;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,17 @@ public class SpotifyService {
         ArrayList<Token> tokens = new ArrayList<Token>();
         tokenRepository.findAll().forEach(token -> tokens.add(token));
         return tokens;
+    }
+
+    public Token getNewestToken() {
+        Token newestToken = new Token();
+        ArrayList<Token> tokens = new ArrayList<Token>();
+        tokenRepository.findAll().forEach(token -> tokens.add(token));
+        newestToken = tokens
+            .stream()
+            .max(Comparator.comparing(Token::getId))
+            .orElseThrow(NoSuchElementException::new);
+        return newestToken;
     }
 
     public Token getTokenById(Integer id) {
